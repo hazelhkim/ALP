77b6534a4e16b4541cb90f16db9457b74a172b79
Hazel Kim <hazel.kimh@gmail.com>
2022-03-18 18:44:04 +0900

    Update huggingface_utils.py

--- huggingface_utils.py
+++ huggingface_utils.py
@@ -1,20 +1,5 @@
 from transformers import *
 
-# HuggingFace Transformers has a unified API
-# for 10 transformer architectures and 30 pretrained weights.
-#          Model          | Tokenizer          | Pretrained model config
-MODELS = [(TFAlbertModel, AlbertTokenizer, AlbertConfig),
-          (TFBertModel, BertTokenizer, BertConfig),
-          (TFElectraModel, ElectraTokenizer, ElectraConfig),
-          (TFOpenAIGPTModel, OpenAIGPTTokenizer, OpenAIGPTConfig),
-          (TFGPT2LMHeadModel, GPT2Tokenizer, GPT2Config),
-          (TFCTRLModel, CTRLTokenizer, CTRLConfig),
-          (TFTransfoXLModel,  TransfoXLTokenizer, TransfoXLConfig),
-          (TFXLNetModel, XLNetTokenizer, XLNetConfig),
-          (TFXLMModel, XLMTokenizer),
-          (TFDistilBertModel, DistilBertTokenizer, DistilBertConfig),
-          (TFRobertaModel, RobertaTokenizer, RobertaConfig),
-          (TFXLMRobertaModel, XLMRobertaTokenizer, XLMRobertaConfig),
-          (TFMobileBertForMaskedLM, MobileBertTokenizer, MobileBertConfig),
-          (TFFunnelForMaskedLM, FunnelTokenizer, FunnelConfig)
-          ]
+# Can add other pre-trained models listed by HuggingFace Transformers.
+# ( Model| Tokenizer | Pretrained model config )
+MODELS = [(TFBertModel, BertTokenizer, BertConfig)]
